519f72a623740a704e2e30026835d8ea1dc17473
unknown <devansh@uwo24.com>
2026-07-13 16:18:27 +0530

    chore: update phone abha script with new whatsapp credentials

--- update_phone_abha.py
+++ update_phone_abha.py
@@ -1,11 +1,12 @@
 import os, django; os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings'); django.setup(); from api.models import Client, User; 
-for c in Client.objects.all():
-    c.whatsapp_phone_number_id = ''
-    c.save()
 try:
     user = User.objects.get(email='abha@uwo24.com')
     if user.client:
-        user.client.whatsapp_phone_number_id = '1125247290680369'
+        user.client.whatsapp_phone_number_id = '1144355915438778'
+        user.client.whatsapp_waba_id = '947532301669617'
+        user.client.phone_number = '+918358990909'
+        user.client.whatsapp_access_token = 'EAAOFcZAhTdBUBR6ZCurFTxLGsXX5OZArXnyRytzrXwUoTjnm02sCmwlkeh53PA0QvQ060fXtD57MeYIbNEQX5R9GWMvOwaZCudOcO8vajRe2iuOfgqlDQScyanDfuzbh9w0esx4I6IOiFo2IKRbfETZACcxhJEL5dShBP21ZBO81xHbvoHQ9S1EsP1IReGRxC7ngZDZD'
+        user.client.whatsapp_verify_token = 'aisaconnect_secure_token'
         user.client.save()
         print('Updated successfully for abha@uwo24.com')
     else:
